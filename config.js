d29c87b40724c07109c3060d2ac4a4e2057876d2
Rehan Saeed <saeedrehan3@gmail.com>
2019-12-25 13:51:16 +0500

    changes

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  secret: 'MY SECRET',
+  DB_URI_DEV: 'mongodb+srv://mongoUser:mongoUser@cluster0-ai8sm.mongodb.net/neil-test-ecommerce?retryWrites=true&w=majority',
+  DB_URI: 'mongodb://mongodb0.example.com:27017/admin'
+};
